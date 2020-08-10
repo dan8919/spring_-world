21a11a651253adbd0f9ac216328c434965893c54
dan8919 <62277410+dan8919@users.noreply.github.com>
2020-08-10 11:25:14 +0900

    Update HomeController.java

--- Hello/src/main/java/kr/co/ca/HomeController.java
+++ Hello/src/main/java/kr/co/ca/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 //수정test1
+//수정 test2 git
 /**
  * Handles requests for the application home page.
  */
